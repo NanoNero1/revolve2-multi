e41333f572f5aa426f303929ef444fbef7bc3f94
NanoNero1 <dimitri.kachler@gmail.com>
2023-04-16 16:08:12 +0200

    test

--- examples/sanity3P/optimize.py
+++ examples/sanity3P/optimize.py
@@ -16,7 +16,7 @@ import os
 async def main() -> None:
     """Run the optimization process."""
     # number of initial mutations for body and brain CPPNWIN networks
-    NUM_INITIAL_MUTATIONS = 20
+    NUM_INITIAL_MUTATIONS = 10
 
     SIMULATION_TIME = 10
     SAMPLING_FREQUENCY = 5
